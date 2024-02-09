cc1af30429e5d2ff026e45765cb11c642db92866
Sergii Kirianov <s.kirianov@gmail.com>
2024-02-09 16:52:44 +0200

    installing sdk

---
+++ apps/storefront/sdk/sdk.config.ts
@@ -0,0 +1,17 @@
+import { sapccModule } from '@vsf-enterprise/sapcc-sdk';
+import { CreateSdkOptions, createSdk } from "@vue-storefront/next";
+
+const options: CreateSdkOptions = {
+  middleware: {
+    apiUrl: "http://localhost:8181",
+  }
+};
+
+export const { getSdk } = createSdk(
+  options,
+  ({ buildModule, middlewareUrl, getRequestHeaders }) => ({
+    sapcc: buildModule(sapccModule, {
+      apiUrl: middlewareUrl + "/sapcc",
+    }),
+  }),
+);
