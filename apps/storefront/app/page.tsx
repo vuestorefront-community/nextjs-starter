f66a350194f0d4aef924be44cd166fd690d51425
Sergii Kirianov <s.kirianov@gmail.com>
2024-02-12 15:44:35 +0200

    first request

--- apps/storefront/app/page.tsx
+++ apps/storefront/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
+import { getSdk } from "../sdk/sdk.config"
+
+const sdk = getSdk();
+
+export default async function Page() {
+  const { products } = await sdk.sapcc.searchProduct({});
+
+  console.log(products?.map((product) => product.name));
+
   return <div>Page</div>
 }
